953f589e46beefd817da60b4019ada2fdacffdc5
Jeffrey Wu <wu19930628@gmail.com>
2026-01-21 10:46:28 +0800

    chore: adjust github url with plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # about: Automatically increment fake registration count daily
 # version: 1.0.0
 # authors: Jeffrey
-# url: https://github.com/your-repo
+# url: https://github.com/b89k57w62/discourse-auto-register-count
 
 after_initialize do
   load File.expand_path('../jobs/scheduled/auto_increment_register_count.rb', __FILE__)
